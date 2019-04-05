045ac453fef814e64d016e3d4848d4025544fb8c
Joey Zhao <joey@localhost.localdomain>
2019-04-04 20:44:08 -0400

    Implement swipe up and down for info card

--- app/src/main/java/com/example/a500pxpopularphotos/FullscreenImageActivity.java
+++ app/src/main/java/com/example/a500pxpopularphotos/FullscreenImageActivity.java
@@ -5,6 +5,13 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +22,11 @@ import com.ortiz.touchview.TouchImageView;
 public class FullscreenImageActivity extends BaseActivity {
     TouchImageView mFullscreenImage;
     public static String IMAGE_URL;
+    GestureDetectorCompat mFling;
+    CardView mInfoCard;
+
+    boolean mCardRaised = false;
+    int mDisplayHeight;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,9 +34,26 @@ public class FullscreenImageActivity extends BaseActivity {
         setContentView(R.layout.activity_fullscreen_image);
 
         mFullscreenImage = (TouchImageView) findViewById(R.id.fullscreen_image);
+        mInfoCard = (CardView) findViewById(R.id.fullscreen_card);
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mDisplayHeight = displayMetrics.heightPixels;
+
+        mFullscreenImage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("UI", "on touch event root");
+                mFling.onTouchEvent(event);
+                return true;
+            }
+        });
 
         Intent intent = getIntent();
         String imageUrl = intent.getStringExtra(IMAGE_URL);
+
+        mFling = new GestureDetectorCompat(this, new FlingGestureListener());
 
         Glide.with(this)
                 .asBitmap()
@@ -35,5 +64,33 @@ public class FullscreenImageActivity extends BaseActivity {
                         mFullscreenImage.setImageBitmap(resource);
                     }
                 });
+    }
+
+    public class FlingGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e2.getY() > e1.getY()) {
+                Log.d("UI", "fling down");
+                if (mCardRaised) {
+                    lowerCard();
+                    mCardRaised = false;
+                }
+            } else if (e1.getY() > e2.getY()) {
+                Log.d("UI", "fling up");
+                if (!mCardRaised) {
+                    raiseCard();
+                    mCardRaised = true;
+                }
+            }
+            Log.d("UI", "fling unknown");
+            return true;
+        }
+    }
+
+    private void raiseCard() {
+        mInfoCard.animate().translationY(0);
+    }
+    private void lowerCard() {
+        mInfoCard.animate().translationY(mDisplayHeight);
     }
 }
