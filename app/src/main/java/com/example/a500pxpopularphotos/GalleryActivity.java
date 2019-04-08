be4a16efe6c73bd4dce7f203bb294a37d254ebc7
Joey Zhao <joey@localhost.localdomain>
2019-04-08 14:22:18 -0400

    refactor scrollingactivity to galleryactivity

--- app/src/main/java/com/example/a500pxpopularphotos/GalleryActivity.java
+++ app/src/main/java/com/example/a500pxpopularphotos/GalleryActivity.java
@@ -23,7 +23,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ScrollingActivity extends BaseActivity {
+public class GalleryActivity extends BaseActivity {
     SwipeRefreshLayout mRefresh;
     int mGalleryItemWidth;
     int mScreenHeight;
